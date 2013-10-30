a188c6c7e4cba510c020cf7145081279550cbf18
Paul Clark <pdclark@brainstormmedia.com>
2013-10-30 00:57:15 -0800

    Replace license comment block with license header

--- plugin.php
+++ plugin.php
@@ -7,29 +7,9 @@ Description: Search and install WordPress plugins from Github.
 Version: 0.1
 Author: Brainstorm Media
 Author URI: http://brainstormmedia.com/
+License: GPLv2
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
 */
-
-/**
- * Copyright (c) 2013 Brainstorm Media. All rights reserved.
- *
- * Released under the GPL license
- * http://www.opensource.org/licenses/gpl-license.php
- *
- * This is an add-on for WordPress
- * http://wordpress.org/
- *
- * **********************************************************************
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- * **********************************************************************
- */
 
 /**
  * Used for localization text-domain, which must match wp.org slug.
